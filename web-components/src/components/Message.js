4aea17f4934ddfb6580e09b5af823da36f541c8d
bshelkhonov <shelbulat@mail.ru>
2019-10-15 01:48:08 +0300

    added tick

--- web-components/src/components/Message.js
+++ web-components/src/components/Message.js
@@ -15,21 +15,33 @@ template.innerHTML = `
     word-wrap: break-word;
   }
 
+  .message-status {
+    display: flex;
+    justify-content: flex-end;
+    right: 1px;
+  }
+
   .time-stamp {
     font-size: 1.5vh;
     text-align: right;
+    margin-right: 1vw;
   }
 
   .read-status {
-    height: 5vh;
-    width: 5vh;
-    fill: white;
+    height: 1.5vh;
+    width: 1.5vh;
+    fill: #8e24aa;
   }
 
   </style>
   <div class='message-box'>
     <div class='message-text'></div>
-    <div class='time-stamp'></div>
+    <div class='message-status'>
+      <div class='time-stamp'></div>
+      <div class='read-status'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 594.149 594.149"><path d="M448.8 161.925l-35.7-35.7-160.65 160.65 35.7 35.7 160.65-160.65zm107.099-35.7l-267.75 270.3-107.1-107.1-35.7 35.7 142.8 142.8 306-306-38.25-35.7zM0 325.125l142.8 142.8 35.7-35.7-142.8-142.8-35.7 35.7z"/></svg>
+      </div>
+    </div>
   </div>
 
 `;
